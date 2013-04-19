e239815b42748f7185856ef2ff3562c057306e42
German M. Bravo <german.mb@deipi.com>
2013-04-19 11:14:10 -0500

    ImageSpecFieldFile properly sends the name (pre-calculated using _make_*)

--- imagekit/models/fields/files.py
+++ imagekit/models/fields/files.py
@@ -9,31 +9,37 @@ from ...utils import suggest_extension
 
 class ImageSpecFieldFile(ImageFieldFile):
     def __init__(self, instance, field, attname):
-        super(ImageSpecFieldFile, self).__init__(instance, field, None)
+        self._make_source_file(instance, field, attname)
+        self._make_name(instance, field, attname)
+        super(ImageSpecFieldFile, self).__init__(instance, field, self.name)
         self.attname = attname
         self.storage = self.field.storage or self.source_file.storage
 
-    @property
-    def source_file(self):
-        field_name = getattr(self.field, 'image_field', None)
+    def _make_source_file(self, instance, field, attname):
+        field_name = getattr(field, 'image_field', None)
         if field_name:
-            field_file = getattr(self.instance, field_name)
+            field_file = getattr(instance, field_name)
         else:
-            image_fields = [getattr(self.instance, f.attname) for f in \
-                    self.instance.__class__._meta.fields if \
+            image_fields = [getattr(instance, f.attname) for f in \
+                    instance.__class__._meta.fields if \
                     isinstance(f, ImageField)]
             if len(image_fields) == 0:
                 raise Exception('%s does not define any ImageFields, so your' \
                         ' %s ImageSpecField has no image to act on.' % \
-                        (self.instance.__class__.__name__, self.attname))
+                        (instance.__class__.__name__, attname))
             elif len(image_fields) > 1:
                 raise Exception('%s defines multiple ImageFields, but you' \
                         ' have not specified an image_field for your %s' \
-                        ' ImageSpecField.' % (self.instance.__class__.__name__,
-                        self.attname))
+                        ' ImageSpecField.' % (instance.__class__.__name__,
+                        attname))
             else:
                 field_file = image_fields[0]
+        self._source_file = field_file
         return field_file
+
+    @property
+    def source_file(self):
+        return self._source_file
 
     def _require_file(self):
         if not self.source_file:
@@ -104,8 +110,7 @@ class ImageSpecFieldFile(ImageFieldFile):
         new_name = '%s_%s%s' % (filename, specname, extension)
         return os.path.join('cache', filepath, new_name)
 
-    @property
-    def name(self):
+    def _make_name(self, instance, field, attname):
         """
         Specifies the filename that the cached image will use. The user can
         control this by providing a `cache_to` method to the ImageSpecField.
@@ -113,30 +118,37 @@ class ImageSpecFieldFile(ImageFieldFile):
         """
         name = getattr(self, '_name', None)
         if not name:
-            filename = self.source_file.name
+            try:
+                filename = self.source_file.name
+            except AttributeError:
+                filename = None
             new_filename = None
             if filename:
-                cache_to = self.field.cache_to or self._default_cache_to
+                cache_to = field.cache_to or self._default_cache_to
 
                 if not cache_to:
                     raise Exception('No cache_to or default_cache_to value'
                             ' specified')
                 if callable(cache_to):
                     suggested_extension = suggest_extension(
-                            self.source_file.name, self.field.generator.format)
+                            self.source_file.name, field.generator.format)
                     new_filename = force_unicode(
                             datetime.datetime.now().strftime(
-                            smart_str(cache_to(self.instance,
-                            self.source_file.name, self.attname,
-                            suggested_extension))))
+                                smart_str(cache_to(instance,
+                                self.source_file.name, attname,
+                                suggested_extension))))
                 else:
                     dir_name = os.path.normpath(
                             force_unicode(datetime.datetime.now().strftime(
-                            smart_str(cache_to))))
+                                smart_str(cache_to))))
                     filename = os.path.normpath(os.path.basename(filename))
                     new_filename = os.path.join(dir_name, filename)
 
             self._name = new_filename
+        return self._name
+
+    @property
+    def name(self):
         return self._name
 
     @name.setter
